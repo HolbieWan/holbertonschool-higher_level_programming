d76fcdf61148b749415cf300895db115605a3ceb
HolbieWan <8841@holbertonstudents.com>
2024-05-07 10:03:41 +0000

    Added file: 2-print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-# Print lowercase letters from 'a' to 'z'
-for i in range(ord('a'), ord('z')+1):
-    print(chr(i), end="")
+for letter in 'abcdefghijklmnopqrstuvwxyz':
+    print(letter, end="")
