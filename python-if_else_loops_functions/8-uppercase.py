bdf0aeee2aa3ed2ebbfb82d746e17ad333661f3c
HolbieWan <8841@holbertonstudents.com>
2024-05-08 18:06:43 +0000

    Corrected style: 7-islower.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 c = "test"
 def uppercase(str):
-	if (ord(c) > 96 and ord(c) < 123):
-		c = chr(ord(c) - 32)
-	print("{}" .format(c))
+    if (ord(c) > 96 and ord(c) < 123):
+        c = chr(ord(c) - 32)
+    print("{}" .format(c))
