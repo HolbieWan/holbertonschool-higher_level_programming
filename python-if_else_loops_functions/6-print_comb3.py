8bd40e28e061d71f5083392fc15dd349ff620b6f
HolbieWan <8841@holbertonstudents.com>
2024-05-08 18:04:09 +0000

    Updated 6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for decimal in range (10):
-	for units in range (decimal + 1, 10):
-		print("{}{}" .format(decimal, units), end=", ")
+for decimal in range(8):
+    for units in range(decimal + 1, 10):
+        print("{}{}" .format(decimal, units), end=", ")
 print("89")
