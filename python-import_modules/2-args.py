90de25d57c627b81088e8fbdbdb73a031e261b5e
HolbieWan <8841@holbertonstudents.com>
2024-05-09 13:07:49 +0000

    Added file: 2-args.py

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+
+
+def print_arguments(*args):
+
+    num_arguments = len(args)
+    if num_arguments == 0:
+        print("{} argument." .format(num_arguments))
+    else:
+        print("{} arguments:" .format(num_arguments))
+    for i in range(1, num_arguments):
+        print("{}: {}".format(i, args[i]))
+
+
+# Call the function with command-line arguments
+print_arguments(*sys.argv[1:])
