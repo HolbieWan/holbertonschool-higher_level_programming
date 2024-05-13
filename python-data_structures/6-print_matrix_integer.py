4aac5ed27222fff21f0316932c50c0ea9ff21f40
HolbieWan <8841@holbertonstudents.com>
2024-05-13 20:05:17 +0000

    Added files: 6-main.py, 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,8 @@
 def print_matrix_integer(matrix=[[]]):
     for inner_list in matrix:
         for element in inner_list:
-            print("{:d}" .format(element), end="")
+            if element == inner_list[-1]:
+                print("{:d}" .format(element), end="")
+            else:
+                print("{:d}" .format(element), end=" ")
         print()
