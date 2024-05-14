b95d6c5e59fa568237dcdc0ec38ea33eea5a80be
HolbieWan <8841@holbertonstudents.com>
2024-05-14 17:06:04 +0000

    Added files: 1-search_replace.py, 1-main.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,13 +2,10 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-
     for i in matrix:
         new_list = []
-
         for x in i:
             new_list.append(x ** 2)
-
         new_matrix.append(new_list)
 
     return new_matrix
