7e5a68f9c3a73c93e58d1ac28ed48844d2081ba3
HolbieWan <8841@holbertonstudents.com>
2024-05-15 16:15:32 +0000

    Added files: 3-safe_print_division.py, 3-main.py

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
 def safe_print_division(a, b):
-    div  = 0
     try:
         div = a / b
-
     except (ZeroDivisionError, TypeError):
         div = None
-
     finally:
         print("Inside result: {}".format(div))
         return div
