a6b0272df7ab323ba82e1e4f1738a20eaf86bd3f
HolbieWan <8841@holbertonstudents.com>
2024-05-15 09:57:04 +0000

    Added files: 10-best_score.py, 10-main.py

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    roman_dict = {'I':1, 'IV':4, 'X':10, 'IX':9, 'L':50, 'D':500, 'C':100}
+    for key in roman_string:
+        if  == roman_string:
+            count += roman_dict.values
+            return count
+        
