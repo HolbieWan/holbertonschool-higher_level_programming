453c84fa9ac8adc3f58f647378df7e851eb5dd9a
HolbieWan <8841@holbertonstudents.com>
2024-05-15 11:37:56 +0000

    Added files: 12-roman_to_int.py, 12-main.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    roman_dict = {'I':1, 'IV':4, 'X':10, 'IX':9, 'L':50, 'D':500, 'C':100}
-    for key in roman_string:
-        if  == roman_string:
-            count += roman_dict.values
-            return count
-        
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500}
+    total = 0
+    i = 0
+
+    while i < len(roman_string):
+        value = roman_dict[roman_string[i]]
+
+        if i + 1 < len(roman_string):
+            if roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]:
+                total -= value
+            else:
+                total += value
+
+        i += 1
+
+    return total
