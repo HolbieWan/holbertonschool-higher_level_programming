771dc88a8c3dcfcf098f01e19828f640db3958d1
HolbieWan <8841@holbertonstudents.com>
2024-05-15 15:36:11 +0000

    Added files: 2-safe_print_list_integers.py, 2-main.py

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,8 @@
-#!/ussr/bin/python3
+#!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
     nb_printed = 0
+
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
