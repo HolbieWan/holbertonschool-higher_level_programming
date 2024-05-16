dcc6f6f2ad76904a49794a7163b70ab704006e45
HolbieWan <8841@holbertonstudents.com>
2024-05-16 14:56:44 +0000

    Added files: 4-square.py, 4-main.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with an optional size.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Function that returns the area of a square of size"""
+        return self.__size ** 2
+
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
