b5c224bf3bbc04f21ba93c54ebe8f14f8c87a6c5
HolbieWan <8841@holbertonstudents.com>
2024-05-16 12:48:34 +0000

    Added files: 1-square.py, 1-main.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        size: The size of the square.
+    """
+
     def __init__(self):
-        self.size
+        """
+        Initializes a new Square instance.
+        """
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
-__import__('0-square')
+__import__('0-square') 
+"""
+    Represents a square with a given size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new Square instance with the given size.
+    """
 
 class Square:
     def __init__(self, size):
