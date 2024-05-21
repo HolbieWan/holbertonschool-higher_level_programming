c97b66299e3c88ff888daea9744a244b0824963c
HolbieWan <8841@holbertonstudents.com>
2024-05-21 17:04:35 +0000

    Added files: 4-print_square.py, 4-main.py, 4-print_square.txt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""Print_square"""
+
 
 def print_square(size):
+    """Function that prints a square with the char # """
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if (size < 0):
-          raise ValueError("size must be >= 0")
-    if (size == 0):
-        print("")
-        return
+        raise ValueError("size must be >= 0")
+
     for i in range(size):
         for i in range(size):
             print("#", end="")
