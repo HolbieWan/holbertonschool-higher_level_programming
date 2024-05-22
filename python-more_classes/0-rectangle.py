cbbe412e3e6ead1404ab820f04d2f7a4c18f8f90
HolbieWan <8841@holbertonstudents.com>
2024-05-22 09:45:33 +0000

    Added files: 0-rectangle.py, README.md, 0-main.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -3,14 +3,4 @@
 
 class Rectangle:
     """Represent a rectangle with a name"""
-
-    lenght = 0
-    height = 0
-
-    count = 0
-
-    def __init__(self, name):
-        self.name = name
-
-        Rectangle.count += 1
-
+    pass
