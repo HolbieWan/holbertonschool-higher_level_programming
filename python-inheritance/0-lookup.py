d0c982c12fc618c9da9c15f6f014bdc47d0d98bb
HolbieWan <8841@holbertonstudents.com>
2024-05-23 09:58:24 +0000

    Added files: 0-lookup.py, 0-main.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -2,5 +2,8 @@
 """Function lookup"""
 
 def lookup(obj):
-    """function that returns the list of available attributes and methods of an object"""
+    """
+    function that returns the list of available attributes
+      and methods of an object
+    """
     return dir(obj)
