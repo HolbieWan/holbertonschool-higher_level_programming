6417d9822d4bb0b675e81e9ae63a642136e41986
HolbieWan <8841@holbertonstudents.com>
2024-05-23 13:13:43 +0000

    Added files: 6-main.py, 6-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module ('6-base_geometry')"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+    def area(self):
+        """Method that raises an Exception with the message area() is not implemented""" 
+        raise Exception("area() is not implemented")
