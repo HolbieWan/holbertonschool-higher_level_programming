d36c367dd690249943eaf9b9ec4d3bedbf4efa4b
HolbieWan <8841@holbertonstudents.com>
2024-05-24 08:34:59 +0000

    Added files: 11-main.py, 11-square.py

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Integer validator"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """representation of a square"""
+
+    def __init__(self, size):
+        """
+        Method to initialize square size with
+        int and positiv values
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """custom str method for print the rectangle"""
+        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)
+
+    def area(self):
+        """computes the area of the retangle"""
+        return self.__size ** 2
