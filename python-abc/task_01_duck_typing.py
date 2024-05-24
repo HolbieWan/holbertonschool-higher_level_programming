afea92da66104770e7eecd08a44bf0df06092a0e
HolbieWan <8841@holbertonstudents.com>
2024-05-24 20:59:01 +0000

    Added files: task_01_duck_typing.py, main_01_duck_typing.py

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -27,14 +27,10 @@ class Circle(Shape):
         self.radius = radius
 
     def area(self):
-        if self.radius < 0:
-            raise ValueError("Radius cannot be negative")
         py_value = math.pi
         return (self.radius ** 2 * py_value)
 
     def perimeter(self):
-        if self.radius < 0:
-            raise ValueError("Radius cannot be negative")
         py_value = math.pi
         return (self.radius * 2 * py_value)
 
@@ -50,18 +46,10 @@ class Rectangle(Shape):
         self.height = height
 
     def area(self):
-        if self.width < 0:
-            raise ValueError("Width cannot be negative")
-        if self.height < 0:
-            raise ValueError("Height cannot be negative")
         py_value = math.pi
         return (self.width * self.height)
 
     def perimeter(self):
-        if self.width < 0:
-            raise ValueError("Width cannot be negative")
-        if self.height < 0:
-            raise ValueError("Height cannot be negative")
         return (self.height + self.width) * 2
 
 
