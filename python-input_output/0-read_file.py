35d1002e08b106dff32f0eb340776751f5ecc606
HolbieWan <8841@holbertonstudents.com>
2024-05-27 09:42:50 +0000

    Added files: 0-read_file.py, O-main.py, my_file_0.txt, README.md

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read(), end="")
+        read_file = file.read()
+        print(read_file, end="")
