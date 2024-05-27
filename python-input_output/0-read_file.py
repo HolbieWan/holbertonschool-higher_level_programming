9cba5053057d7c82262c103b548f6c66fb7b6d1c
HolbieWan <8841@holbertonstudents.com>
2024-05-27 08:21:46 +0000

    Added files: 0-read_file.py, O-main.py, my_file_0.txt, README.md

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,5 +4,5 @@
 
 def read_file(filename=""):
     """Reads a text file (UTF8) and prints it to stdout."""
-    with open(filename, mode="r", encoding="Utf-8") as file:
+    with open(filename, mode="r", encoding="utf-8") as file:
         print(file.read(), end="")
---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""write_file Module"""
+
+def write_file(filename="", text=""):
+    with open(filename, mode="w", encoding="uft_8")
