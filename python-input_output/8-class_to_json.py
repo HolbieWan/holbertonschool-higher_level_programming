e55f5e6ae5ca91b052be15360f3d289800156ece
HolbieWan <8841@holbertonstudents.com>
2024-05-27 13:40:10 +0000

    Added files: 8-main.py,8-main_2.py, 8-my_class_2.py, 8-class_to_json.py

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Class_to_json Module"""
 
+
 def class_to_json(obj):
     """function that returns the dictionary description with simple
     data structure (list, dictionary, string, integer and boolean)
