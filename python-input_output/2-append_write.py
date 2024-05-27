672d4d3686f3143298c2df965192d414c0aa6104
HolbieWan <8841@holbertonstudents.com>
2024-05-27 09:00:26 +0000

    Added files: 2-main.py, 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,7 +3,8 @@
 
 
 def append_write(filename="", text=""):
-    """appends a string to a text file (UTF8) and returns the number of characters written."""
+    """appends a string to a text file (UTF8) and
+    returns the number of characters written."""
     with open(filename, mode="a", encoding="utf-8") as file:
         nb_chars = file.write(text)
     return nb_chars
