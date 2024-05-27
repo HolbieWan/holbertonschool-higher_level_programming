166eee78ee8b1775ce23ca2ea5af1127c437cf0c
HolbieWan <8841@holbertonstudents.com>
2024-05-27 08:30:26 +0000

    Added files: 1-main.py, 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """write_file Module"""
 
+
 def write_file(filename="", text=""):
-    with open(filename, mode="w", encoding="uft_8")
+    """Function that overrides the string: text
+    and returns the number of added char"""
+    with open(filename, mode="w", encoding="utf-8") as file:
+        file.write(text)
+    return len(text)
