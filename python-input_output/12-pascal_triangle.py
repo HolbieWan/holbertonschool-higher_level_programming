9116c2d01ffd798ae2bc54bbba2b8c7b38b3036e
HolbieWan <8841@holbertonstudents.com>
2024-05-28 09:32:45 +0000

    Added files: 12-main.py, 12-pascal_triangle.py

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Pascal Triangle Module"""
+
+
+def pascal_triangle(n):
+    """Function that returns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return
+
+    pascal_triangle = [[1]]
+
+    for i in range(1, n):
+        new_row = [1]
+        for j in range(1, i):
+            new_value = \
+                pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j]
+            new_row.append(new_value)
+        new_row.append(1)
+        pascal_triangle.append(new_row)
+
+    return (pascal_triangle)
