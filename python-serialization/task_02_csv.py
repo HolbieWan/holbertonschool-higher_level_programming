7ec71464a7a2ea94c02716b54ee2da4c3a52b598
HolbieWan <8841@holbertonstudents.com>
2024-05-28 15:33:04 +0000

    Added files: 0-main.py, task_02_csv.py, data.json

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -7,16 +7,23 @@ import json
 
 def convert_csv_to_json(filename):
     """Method to convert a csv file into json"""
-    csv_list = []
-    with open(filename, "r", encoding="utf-8", newline='') as csv_file:
-        csv_dict = csv.DictReader(csv_file)
-        for row in csv_dict:
-            csv_list.append(row)
+    try:
+        csv_list = []
+        with open(filename, "r", encoding="utf-8", newline='') as csv_file:
+            csv_dict = csv.DictReader(csv_file)
+            for row in csv_dict:
+                csv_list.append(row)
 
-    with open("data.json", mode='w') as json_file:
-        json_file.write('[\n')
-        for i, item in enumerate(csv_list):
-            json.dump(item, json_file)
-            if i < len(csv_list) - 1:
-                json_file.write(',\n')
-        json_file.write('\n]')
+        with open("data.json", mode='w') as json_file:
+            json_file.write('[\n')
+            for i, item in enumerate(csv_list):
+                json.dump(item, json_file)
+                if i < len(csv_list) - 1:
+                    json_file.write(',\n')
+            json_file.write('\n]')
+
+    except FileNotFoundError:
+        return False
+    except Exception:
+        return False
+    return True
