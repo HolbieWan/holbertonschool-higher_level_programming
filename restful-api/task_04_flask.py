3bc46c097ab29eb77c451b29d3b4d97face2cb5b
HolbieWan <8841@holbertonstudents.com>
2024-06-02 11:34:57 +0000

    Added file: task_04_flask.py

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Creating a simple server Module"""
 
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 
 app = Flask(__name__)
 
@@ -23,16 +23,19 @@ def home():
     """Default endpoint for homepage"""
     return "Welcome to the Flask API!"
 
+
 @app.route('/data')
 def data():
     """Endpoint returning a JSON response with all usernames"""
     names = [user["name"] for user in users.values()]
     return jsonify(names)
 
+
 @app.route('/status')
 def status():
     """Default endpoint returning the status of the API"""
     return "OK"
+
 
 @app.route('/users/<username>')
 def get_user(username):
@@ -42,5 +45,18 @@ def get_user(username):
         abort(404, description="User not found")
     return jsonify(user)
 
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    """Endpoint to add a new user"""
+    new_user = request.json
+    username = new_user['username']
+    users[username] = new_user
+    return jsonify({
+        'message': 'User added',
+        'user': new_user
+    })
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8000)
