db1c12884c6bd15f37b39cc683faa237f9d2e408
HolbieWan <8841@holbertonstudents.com>
2024-06-17 16:30:31 +0000

    Add: 3-list_tables.sql, 4-first_table.sql, 5-full_table.sql, 6-list_values.sql, 7-insert_value.sql, 8-count_89.sql, 9-full_creation.sql

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,2 @@
+-- Command to add a row to a table:
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Command to display the number of records with the id = 89 in the table first_table:
+SELECT COUNT(*) FROM first_table WHERE id = 89;
