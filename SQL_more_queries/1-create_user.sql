2661ab37517a3f225ae5b314bf880e945213d615
HolbieWan <8841@holbertonstudents.com>
2024-06-18 09:23:02 +0000

    Update: 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Command to create a new user and give it the SELECT privilege on the database hbtn_0d_2:
 CREATE USER if not EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
