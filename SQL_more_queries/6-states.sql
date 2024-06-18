4ead427bbab122d09b008961b91ce8940ea343af
HolbieWan <8841@holbertonstudents.com>
2024-06-18 12:13:36 +0000

    Add: 6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,10 @@
 -- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa)
-CREATE DATABASE if not EXISTS hbtn_0d_usa;
-CREATE TABLE if not EXISTS states in database hbtn_0d_usa (
-    id INT auto generated unique not null,
-    name VARCHAR(256) not null,
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
