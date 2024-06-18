bf3c98bc0c34351505933ff844d4ec304a822816
HolbieWan <8841@holbertonstudents.com>
2024-06-18 07:35:52 +0000

    Update: 2-remove_database.sql, 11-best_score.sql

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,3 @@
 -- Command to list all records of the table second_table where the score is greater than 10:
-SELECT score, name FROM second_table WHERE score >= 10;
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
