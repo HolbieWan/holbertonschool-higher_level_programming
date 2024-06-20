1f08dcae3981e2f45b58e63085489b55228285ea
HolbieWan <8841@holbertonstudents.com>
2024-06-20 10:32:24 +0000

    Update: 0-select_states.py, README.md

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,36 +3,39 @@
 
 import MySQLdb
 
-# Database connection parameters
-host = 'localhost'
-user = ''
-password = ''
-database = 'hbtn_0e_0_usa'
-port = 3306
+def get_all_states():
+    """Method that lists all states from the database hbtn_0e_0_usa"""
 
-# Establish the connection
-conn = MySQLdb.connect(
-    host=host,
-    user=user,
-    passwd=password,
-    db=database
-)
+    # Database connection parameters
+    host = 'localhost'
+    user = ''
+    password = ''
+    database = 'hbtn_0e_0_usa'
+    port = 3306
 
-# Create a cursor object
-cursor = conn.cursor()
+    # Establish the connection
+    conn = MySQLdb.connect(
+        host=host,
+        user=user,
+        passwd=password,
+        db=database
+    )
 
-# Perform a SELECT query
-my_query = "SELECT * FROM states ORDER BY id ASC"
-cursor.execute(my_query)
-rows = cursor.fetchall()
-print("SELECT Query Results:")
-for row in rows:
-    print(row)
+    # Create a cursor object
+    cursor = conn.cursor()
 
-# Close the cursor and connection
-cursor.close()
-conn.close()
+    # Perform a SELECT query
+    my_query = "SELECT * FROM states ORDER BY id ASC"
+    cursor.execute(my_query)
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        print(row)
+
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
-    pass
+    get_all_states()
