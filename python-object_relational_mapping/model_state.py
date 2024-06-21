6914c816ab01e141b0483045948eae90333bce28
HolbieWan <8841@holbertonstudents.com>
2024-06-21 17:38:04 +0000

    6-model_state.py, 6-model_state.sql, model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from sqlalchemy import create_engine, Column, Integer, String
@@ -8,8 +8,14 @@ from sqlalchemy.ext.declarative import declarative_base
 # Create a Base instance for declarative class definitions
 Base = declarative_base()
 
+
 class State(Base):
     """State class that links to the MySQL table states"""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False,
+        unique=True)
     name = Column(String(128), nullable=False)
