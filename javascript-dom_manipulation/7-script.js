5ffe3da28d004f1ed0fb94e38da7f23a6cd95bc2
HolbieWan <8841@holbertonstudents.com>
2024-07-27 16:35:37 +0200

    Add: 8-main.html, 8-script.js

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  fetch("https://swapi-api.hbtn.io/api/films/?format=json")
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
     .then((response) => response.json())
     .then((data) => {
-      const movieList = document.getElementById("list_movies");
+      const movieList = document.getElementById('list_movies');
       for (let i = 0; i < data.results.length; i++) {
-        let listItem = document.createElement("li");
+        let listItem = document.createElement('li');
         listItem.textContent = data.results[i].title;
         movieList.appendChild(listItem);
       }
